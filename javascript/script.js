279b4f363995f1e7e5741f81545b3f8b484ecc09
sendosend <johnsenberdin2930@gmail.com>
2025-07-12 18:08:56 +0800

    Llogical operator

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,32 @@
-// string comparison
+// logical operators
 
-// to se whether a string is greater than another, JS uses the so-called "dictionary" or "lexicographical" order
-// In other words, strings are compared letter by letter
+// OR ||
 
-let a = "abc";
-let b = "abcd";
-console.log(a < b); // true
+// evaluates operands from left to right
+// it converts it to boolean and if its true returns its original value of that operand
+// if there's is no truthy values it returns the last falsy value
 
-// the longer strings are greater
+console.log(2 === 2 || 2 < 1 || 2 > 5); // true
+
+// AND &&
+
+// opposite to OR ||
+// evaluates operands from left to right
+// if converts it to boolean and if its false returns its original value of that operand
+// if there's no falsy value it returns the last truthy value
+
+console.log(2 === 1 && 1 === 1 && "2" > 2); // false
+
+// logical not !
+
+// Converts the operand to boolean type: true or false
+// returns the inverse value
+
+console.log("2" !== 2); // true
+
+// nullish coalescing
+
+// returns the first argument if it's not null/undefined otherwise  the second one
+
+console.log(undefined ?? null); // null
+console.log(2 ?? null);
